d39941a9c178f7f3e0cab0d7f7f0681685a04c67
huntj88 <huntj88@gmail.com>
2015-01-29 18:56:54 -0600

    some table stuff

--- app/src/main/java/com/appsforreddit/thisweekinsceince/WeekFragment.java
+++ app/src/main/java/com/appsforreddit/thisweekinsceince/WeekFragment.java
@@ -8,7 +8,11 @@ package com.appsforreddit.thisweekinsceince;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
+        import android.widget.ImageButton;
         import android.widget.ImageView;
+        import android.widget.TableLayout;
+        import android.widget.TableRow;
+        import android.widget.TextView;
 
         import com.squareup.picasso.Picasso;
 
@@ -39,6 +43,7 @@ public class WeekFragment extends Fragment{
         View v = inflater.inflate(R.layout.temp_layout, container, false);
         ImageView mv = (ImageView) v.findViewById(R.id.imageView);
         Picasso.with(this.getActivity()).load("http://www.futurism.co/wp-content/uploads/2015/01/Science_Jan25th_2015.jpg").into(mv);
+        init(v);
         return v;
     }
 
@@ -55,6 +60,38 @@ public class WeekFragment extends Fragment{
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    public void init(View v){
+        int sdk = android.os.Build.VERSION.SDK_INT;
+        TableLayout ll = (TableLayout) v.findViewById(R.id.table);
+
+        TextView test;
+        ImageView img;
+
+        for (int i = 0; i <8; i++) {
+
+            TableRow row= new TableRow(this.getActivity());
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+            row.setLayoutParams(lp);
+            img = new ImageButton(this.getActivity());
+            Picasso.with(this.getActivity()).load("http://www.futurism.co/wp-content/uploads/2015/01/Science_Jan25th_2015.jpg").into(img);
+
+            if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                img.setBackgroundDrawable(null);
+            } else {
+                img.setBackground(null);
+            }
+
+            test = new TextView(this.getActivity());
+            test.setText("blah blah blah a bunch of stuff about the image maybe");
+            //row.addView(minusBtn);
+            row.addView(img);
+            row.addView(test);
+
+            //row.addView(addBtn);
+            ll.addView(row,i);
+        }
     }
 
 
