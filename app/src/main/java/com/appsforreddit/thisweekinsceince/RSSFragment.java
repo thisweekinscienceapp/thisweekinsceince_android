3b3c5a352347b0ba32b4f92fe33ffb8d5bce6d4c
huntj88 <huntj88@gmail.com>
2015-01-30 18:00:01 -0600

    progress on rss feed

--- app/src/main/java/com/appsforreddit/thisweekinsceince/RSSFragment.java
+++ app/src/main/java/com/appsforreddit/thisweekinsceince/RSSFragment.java
@@ -1,11 +1,12 @@
 package com.appsforreddit.thisweekinsceince;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
-import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,59 +17,33 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-import com.appsforreddit.thisweekinsceince.data.rss.RssFeed;
-import com.appsforreddit.thisweekinsceince.data.rss.RssItem;
-import com.appsforreddit.thisweekinsceince.data.rss.RssReader;
 import com.squareup.picasso.Picasso;
 
-import org.xml.sax.SAXException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link RSSFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link RSSFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class RSSFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<RssItem>> {
+public class RSSFragment extends Fragment{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private ArrayList<String> url = new ArrayList<String>();
     private ArrayList<String> title = new ArrayList<String>();
     private View v;
-    private OnFragmentInteractionListener mListener;
+    private int pageNum=1;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RSSFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RSSFragment newInstance(String param1, String param2) {
-        RSSFragment fragment = new RSSFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
+
 
     public RSSFragment() {
         // Required empty public constructor
@@ -77,10 +52,6 @@ public class RSSFragment extends Fragment implements LoaderManager.LoaderCallbac
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
     }
 
@@ -91,90 +62,41 @@ public class RSSFragment extends Fragment implements LoaderManager.LoaderCallbac
         v = inflater.inflate(R.layout.fragment_rs, container, false);
         //ImageView mv = (ImageView) v.findViewById(R.id.imageView);
         //Picasso.with(this.getActivity()).load("http://www.futurism.co/wp-content/uploads/2015/01/Science_Jan25th_2015.jpg").into(mv);
-
+        getRss();
         return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-//        try {
-//            mListener = (OnFragmentInteractionListener) activity;
-//        } catch (ClassCastException e) {
-//            throw new ClassCastException(activity.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
-        getLoaderManager().initLoader(0, null, this).forceLoad();
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    @Override
-    public Loader<List<RssItem>> onCreateLoader(int id, Bundle args) {
-        return new RSSLoader(getActivity(), "http://www.futurism.co/feed/daily");
-    }
-
-    @Override
-    public void onLoadFinished(android.support.v4.content.Loader<List<RssItem>> loader, List<RssItem> data) {
-        for (RssItem rssItem : data){
-            Log.d("RSS", "rssItem: " + rssItem.getDescription());
-            Log.d("RSS Content",""+rssItem.getContent());
-            url.add(rssItem.getContent());
-            title.add(rssItem.getTitle());
-        }
-        init(v);
-    }
-
-    @Override
-    public void onLoaderReset(android.support.v4.content.Loader<List<RssItem>> loader) {
-
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
     }
 
 
+    @SuppressLint("NewApi")
     public void init(View v) {
         int sdk = android.os.Build.VERSION.SDK_INT;
         TableLayout ll = (TableLayout) v.findViewById(R.id.table);
 
         TextView test;
-        ImageButton img;
+        ImageView img;
         ImageButton button;
         TableRow.LayoutParams imgSize = new TableRow.LayoutParams(250, 250);
         TableRow.LayoutParams upVote = new TableRow.LayoutParams(170, 125);
         upVote.setMargins(0,60,0,0);
         TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < url.size(); i++) {
 
             TableRow row = new TableRow(this.getActivity());
 
@@ -185,7 +107,7 @@ public class RSSFragment extends Fragment implements LoaderManager.LoaderCallbac
             button.setLayoutParams(upVote);
             button.setImageResource(R.drawable.upvote);
 
-            img = new ImageButton(this.getActivity());
+            img = new ImageView(this.getActivity());
             img.setLayoutParams(imgSize);
             Picasso.with(this.getActivity()).load(url.get(i)).into(img);
 
@@ -208,6 +130,77 @@ public class RSSFragment extends Fragment implements LoaderManager.LoaderCallbac
 
 
             ll.addView(row, i);
+        }
+    }
+
+    private void getRss()
+    {
+        if(pageNum<5) {
+            pageNum++;
+            DownloadWebPageTask task = new DownloadWebPageTask();
+            task.execute(new String[]{"http://www.futurism.co/page/" + pageNum});
+        }
+        else
+        {
+            init(v);
+        }
+
+    }
+
+    private class DownloadWebPageTask extends AsyncTask<String, Void, String> {
+
+
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            for (String url2 : urls) {
+                DefaultHttpClient client = new DefaultHttpClient();
+                HttpGet httpGet = new HttpGet(url2);
+                try {
+                    HttpResponse execute = client.execute(httpGet);
+                    InputStream content = execute.getEntity().getContent();
+
+                    BufferedReader buffer = new BufferedReader(
+                            new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+
+                        if(s.contains("<a class=\"title\" href=\""))
+                        {
+                            Scanner scan=new Scanner(s);
+
+                            scan.useDelimiter("\"");
+
+                            scan.next();
+                            scan.next();
+                            scan.next();
+                            scan.next();
+                            scan.next();
+
+                            title.add(scan.next());
+                            scan.close();
+                        }
+                        else if(s.contains("background-image:")) {
+                            Matcher m = Pattern.compile("\\(([^)]+)\\)").matcher(s);
+                            while(m.find()) {
+                                url.add(m.group(1));
+                            }
+                        }
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //textView.setText(textView.getText()+result);
+            Log.d("resultURL", result);
+            getRss();
         }
     }
 
