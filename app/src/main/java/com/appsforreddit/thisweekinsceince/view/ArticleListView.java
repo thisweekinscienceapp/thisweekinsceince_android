e959508f835424e901f0400411c0adfe0d2abd16
Michael Allon <allonmj@gmail.com>
2015-03-21 20:38:50 -0400

    Separate parse_keys from strings.  Add ArticleListView

---
+++ app/src/main/java/com/appsforreddit/thisweekinsceince/view/ArticleListView.java
@@ -0,0 +1,14 @@
+package com.appsforreddit.thisweekinsceince.view;
+
+import com.appsforreddit.thisweekinsceince.data.Article;
+
+import java.util.List;
+
+/**
+ * Created by Michael on 2/12/2015.
+ */
+public interface ArticleListView {
+    public void showArticles(List<Article> articles);
+    public void hideArticles();
+    public void showError(int errorCode, String reason);
+}
